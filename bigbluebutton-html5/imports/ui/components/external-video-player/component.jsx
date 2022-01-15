4e70eb83587e364ad81977f4ad010f59410eafb7
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-01-15 21:25:53 +0900

    camelcase for file player attributes

--- bigbluebutton-html5/imports/ui/components/external-video-player/component.jsx
+++ bigbluebutton-html5/imports/ui/components/external-video-player/component.jsx
@@ -95,8 +95,8 @@ class VideoPlayer extends Component {
       file: {
         attributes: {
           controls: isPresenter ? 'controls' : '',
-          autoplay: 'autoplay',
-          playsinline: 'playsinline',
+          autoPlay: 'autoplay',
+          playsInline: 'playsinline',
         },
       },
       facebook: {
