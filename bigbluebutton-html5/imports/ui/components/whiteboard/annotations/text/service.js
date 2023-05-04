592d7f2b7b678c0e0183801051054b39a8202021
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-05-04 20:50:23 +0900

    Update service.js

--- bigbluebutton-html5/imports/ui/components/whiteboard/annotations/text/service.js
+++ bigbluebutton-html5/imports/ui/components/whiteboard/annotations/text/service.js
@@ -17,11 +17,6 @@ const resetTextShapeActiveId = () => {
     Storage.setItem(DRAW_SETTINGS, drawSettings);
   }
 };
-  
-//const isPresenter = () => {
-//  const currentUser = Users.findOne({ userId: Auth.userID }, { fields: { presenter: 1 } });
-//  return currentUser ? currentUser.presenter : false;
-//};
 
 const activeTextShapeId = () => {
   const drawSettings = Storage.getItem(DRAW_SETTINGS);
