0000694448af4f5e8b6eda7d067aa35b53c1e527
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-05-04 16:11:53 +0900

    Update GetLockSettingsReqMsgHdlr.scala

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/users/GetLockSettingsReqMsgHdlr.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/users/GetLockSettingsReqMsgHdlr.scala
@@ -22,7 +22,6 @@ trait GetLockSettingsReqMsgHdlr {
       disablePubChat = settings.disablePubChat,
       disableNotes = settings.disableNotes,
       hideUserList = settings.hideUserList,
-      hideAnnotations = settings.hideAnnotations,
       lockedLayout = settings.lockedLayout,
       lockOnJoin = settings.lockOnJoin,
       lockOnJoinConfigurable = settings.lockOnJoinConfigurable,
