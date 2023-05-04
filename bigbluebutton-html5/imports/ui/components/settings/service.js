1018f5a8b5855ba27163e41e6320ad6ed9c431fd
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-05-04 20:22:30 +0900

    Update service.js

--- bigbluebutton-html5/imports/ui/components/settings/service.js
+++ bigbluebutton-html5/imports/ui/components/settings/service.js
@@ -3,7 +3,6 @@ import Auth from '/imports/ui/services/auth';
 import Settings from '/imports/ui/services/settings';
 import { notify } from '/imports/ui/services/notification';
 import GuestService from '/imports/ui/components/waiting-users/service';
-import WhiteboardService from '/imports/ui/components/whiteboard/service';
 
 const getUserRoles = () => {
   const user = Users.findOne({
@@ -22,14 +21,6 @@ const showGuestNotification = () => {
 };
 
 const updateSettings = (obj, msg) => {
-  // Update whiteboard mode
-  const dataSaveSetting = obj['dataSaving'];
-  const newWhiteboardMode = {
-    synchronizeWBUpdate: dataSaveSetting.synchronizeWBUpdate,
-    simplifyPencil: dataSaveSetting.simplifyPencil,
-  };
-  WhiteboardService.setWhiteboardMode(newWhiteboardMode);
-  
   Object.keys(obj).forEach(k => (Settings[k] = obj[k]));
   Settings.save();
 
