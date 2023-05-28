eab3df77a608b9d4128911cfb61fb4a3b3bf0aa6
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-05-28 20:44:53 +0900

    Update pushSpeechTranscript.js

--- bigbluebutton-html5/imports/api/captions/server/methods/pushSpeechTranscript.js
+++ bigbluebutton-html5/imports/api/captions/server/methods/pushSpeechTranscript.js
@@ -1,11 +1,67 @@
 import { check } from 'meteor/check';
 import Captions from '/imports/api/captions';
+import Users from '/imports/api/users';
 import { extractCredentials } from '/imports/api/common/server/helpers';
 import Logger from '/imports/startup/server/logger';
 import setTranscript from '/imports/api/captions/server/modifiers/setTranscript';
 import updatePad from '/imports/api/pads/server/methods/updatePad';
+import axios from 'axios';
 
-export default function pushSpeechTranscript(locale, transcript, type) {
+const CAPTIONS_CONFIG = Meteor.settings.public.captions;
+
+function sendTranscript(meetingId, requesterUserId, type, dst, text) {
+  const user = Users.findOne({meetingId, userId: requesterUserId}, { fields: {name: 1}});
+  //console.log("sendTranscript", text);
+  const textWithName = `${user.name}: ${text}`;
+  if (type === 'final') {
+    const textLf = `\n${textWithName}`;
+    updatePad(meetingId, requesterUserId, dst, textLf);
+  }
+  setTranscript(meetingId, dst, textWithName);
+}
+
+function translateText(meetingId, requesterUserId, textOri, type, src, dst) {
+  if ( !CAPTIONS_CONFIG.enableAutomaticTranslation || textOri === "" || !dst || dst === "" || dst === src || dst.replace(/-..$/,'') === src || dst === src.replace(/-..$/,'') ) {
+    sendTranscript(meetingId, requesterUserId, type, dst, textOri);
+  } else {
+    let url = '';
+    if (CAPTIONS_CONFIG.googleTranslateUrl) {
+      url = CAPTIONS_CONFIG.googleTranslateUrl + '/exec?' +
+            'text=' + encodeURIComponent(textOri) + '&source=' + src + '&target=' + dst;
+    } else if (CAPTIONS_CONFIG.deeplTranslateUrl) {
+      url = CAPTIONS_CONFIG.deeplTranslateUrl +
+            '&text=' + encodeURIComponent(textOri) + '&source_lang=' + src.replace(/-..$/,'').toUpperCase() +
+            '&target_lang=' + dst.toUpperCase();
+    } else {
+      Logger.error('Could not get a translation service.');
+      return;
+    }
+
+    axios({
+      method: 'get',
+      url,
+      responseType: 'json',
+    }).then((response) => {
+      if (CAPTIONS_CONFIG.googleTranslateUrl) {
+        const { code, text } = response.data;
+        if (code === 200) {
+          sendTranscript(meetingId, requesterUserId, type, dst, text);
+        } else {
+          Logger.error(`Failed to get Google translation for ${textOri}`);
+        }
+      } else if (CAPTIONS_CONFIG.deeplTranslateUrl) {
+        const { translations } = response.data;
+        if (translations.length > 0 && translations[0].text) {
+          sendTranscript(meetingId, requesterUserId, type, dst, translations[0].text);
+        } else {
+          Logger.error(`Failed to get DeepL translation for ${textOri}`);
+        }
+      }
+    }).catch((error) => Logger.error(`Could not get translation for ${textOri.trim()} on the locale ${dst}: ${error}`));
+  }
+}
+
+export default function pushSpeechTranscript(locale, transcript, type, locales) {
   try {
     const { meetingId, requesterUserId } = extractCredentials(this.userId);
 
@@ -14,22 +70,21 @@ export default function pushSpeechTranscript(locale, transcript, type) {
     check(locale, String);
     check(transcript, String);
     check(type, String);
+    check(locales, Array);
 
-    const captions = Captions.findOne({
-      meetingId,
-      ownerId: requesterUserId,
-      locale,
-      dictating: true,
-    });
+    locales.forEach(function(dstLocale, index) {
+      const caption = Captions.findOne({
+        meetingId,
+        locale: dstLocale,
+      });
 
-    if (captions) {
-      if (type === 'final') {
-        const text = `\n${transcript}`;
-        updatePad(meetingId, requesterUserId, locale, text);
+      if (!caption) {
+        Logger.error(`Could not find caption's pad for meetingId=${meetingId} locale=${locale}`);
+        return;
+      } else {
+        translateText(meetingId, requesterUserId, transcript, type, locale, dstLocale);
       }
-
-      setTranscript(meetingId, locale, transcript);
-    }
+    });
   } catch (err) {
     Logger.error(`Exception while invoking method pushSpeechTranscript ${err.stack}`);
   }
