6ee3414c7ac18c9ba2bcd47c3a02217996471a5d
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-05-28 22:28:59 +0900

    Create setAutoTranslation.js

---
+++ bigbluebutton-html5/imports/api/captions/server/modifiers/setAutoTranslation.js
@@ -0,0 +1,31 @@
+//Do we need this? It just sets "translating"
+import Captions from '/imports/api/captions';
+import Logger from '/imports/startup/server/logger';
+import { check } from 'meteor/check';
+
+export default function setAutoTranslation(meetingId, locale, translating) {
+  check(meetingId, String);
+  check(locale, String);
+  check(translating, Boolean);
+
+  const selector = {
+    meetingId,
+    locale,
+  };
+
+  const modifier = {
+    $set: {
+      translating,
+    }
+  };
+
+  try {
+    const numberAffected = Captions.update(selector, modifier);
+
+    if (numberAffected) {
+      Logger.verbose('Captions: updated pad autoTranslation', { locale });
+    }
+  } catch (err) {
+    Logger.error(`Updating captions pad autoTranslation: ${err}`);
+  }
+}
