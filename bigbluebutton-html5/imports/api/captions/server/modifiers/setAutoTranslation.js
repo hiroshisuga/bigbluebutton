da35728b960360abd8e241dfe1195e0b15b57733
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-05-31 14:13:32 +0900

    Add update "translating" when removing a user

--- bigbluebutton-html5/imports/api/captions/server/modifiers/setAutoTranslation.js
+++ bigbluebutton-html5/imports/api/captions/server/modifiers/setAutoTranslation.js
@@ -3,29 +3,60 @@ import Captions from '/imports/api/captions';
 import Logger from '/imports/startup/server/logger';
 import { check } from 'meteor/check';
 
-export default function setAutoTranslation(meetingId, locale, translating) {
+export default function setAutoTranslation(meetingId, locale, userId, selecting) {
   check(meetingId, String);
   check(locale, String);
-  check(translating, Boolean);
+  check(userId, String);
+  check(selecting, Boolean);
 
   const selector = {
     meetingId,
     locale,
   };
 
-  const modifier = {
-    $set: {
-      translating,
-    }
-  };
+  let modifier;
+  if (selecting) {
+    modifier = {
+      $set: {
+        ['translationDoner.'+userId]: true,
+        translating: true,
+      }
+    };
+  } else {
+    modifier = {
+      $set: {
+        ['translationDoner.'+userId]: false,
+      }
+    };
+  }
 
   try {
-    const numberAffected = Captions.update(selector, modifier);
+    Logger.info(`Adding/Removing (${selecting}) translationDoner ${locale} from ${userId} meeting=${meetingId}`);
 
     if (numberAffected) {
       Logger.verbose('Captions: updated pad autoTranslation', { locale });
     }
   } catch (err) {
     Logger.error(`Updating captions pad autoTranslation: ${err}`);
+  }
+
+  if (!selecting) {
+    const newcap = Captions.findOne({ meetingId, locale }, { fields: { translationDoner: 1 } });
+    const td = newcap.translationDoner;
+    if (!Object.values(td).includes(true)) {
+      const modifier2 = {
+        $set: {
+          translating: false,
+        },
+      };
+      try {
+        const numberAffected = Captions.upsert({ meetingId, locale }, modifier2);
+        if (numberAffected) {
+          Logger.info(`Updating translating to false; ${locale}, meeting=${meetingId} in Captions`);
+        }
+      } catch (err) {
+        Logger.error(`Updating translating: ${err} in Captions`);
+      }
+    }
   }
 }
