f7f8dcbb89cc6646b16f114e2d0e3c8d5c7dbc5b
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2026-07-15 23:39:11 +0900

    replace all targetDoc in whiteboard/compenent

--- bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
@@ -2448,7 +2448,7 @@ const Whiteboard = React.memo((props) => {
   }, [isPresentationDetached, popupWindow]);
 
   React.useEffect(() => {
-    const targetDoc = document;
+    const targetDoc = isPresentationDetached && popupWindow?.document ? popupWindow.document : document;
     const presentationWrapper = targetDoc.querySelector('#presentationInnerWrapper');
     if (!presentationWrapper) return;
     if (!isPresenter) return;
@@ -2504,7 +2504,7 @@ const Whiteboard = React.memo((props) => {
   React.useEffect(() => {
     if (!laserMenuVisible) return;
     
-    const targetDoc = document;
+    const targetDoc = isPresentationDetached && popupWindow?.document ? popupWindow.document : document;
     const presentationWrapper = targetDoc.querySelector('#presentationInnerWrapper');
     if (!presentationWrapper) return;
 
@@ -2523,7 +2523,7 @@ const Whiteboard = React.memo((props) => {
   React.useEffect(() => {
     // compensation at the window edge
     if (!laserMenuVisible) return;
-    const targetDoc = document;
+    const targetDoc = isPresentationDetached && popupWindow?.document ? popupWindow.document : document;
 
     const el = laserMenuRef.current;
     if (!el) return;
@@ -2544,7 +2544,7 @@ const Whiteboard = React.memo((props) => {
   }, [laserMenuVisible]);
 
   React.useEffect(() => {
-    const targetDoc = document;
+    const targetDoc = isPresentationDetached && popupWindow?.document ? popupWindow.document : document;
     if (!isPresenter) return;
     const el = targetDoc.querySelector('.tl-container');
     if (!el) return;
@@ -2656,7 +2656,7 @@ const Whiteboard = React.memo((props) => {
 
   const removeViewerLaser = () => {
     laserElRef.current = null;
-    const targetDoc = document;
+    const targetDoc = isPresentationDetached && popupWindow?.document ? popupWindow.document : document;
     const lasers = targetDoc.querySelectorAll('.bbb-laser-pointer');
     lasers.forEach(el => el.remove());
   };
@@ -2666,7 +2666,7 @@ const Whiteboard = React.memo((props) => {
     if (isPresenter) return;
     //if (isMultiUserActive) return;
 
-    const targetDoc = document;
+    const targetDoc = isPresentationDetached && popupWindow?.document ? popupWindow.document : document;
 
     //Comment out below if we do not want to show laser when a presenter uses drawing tools on mobile devices.
     // Note a problem that the laser remains on the screen after switching to drawing tools.
